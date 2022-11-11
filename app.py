016a711697160fc0b033b53e2b5b2c5dfc859b6f
Sidimar Salla <sidimarsalla0@gmail.com>
2022-11-11 16:43:24 -0300

    Script funcionando

--- app.py
+++ app.py
@@ -2,21 +2,33 @@ import paramiko
 from getpass import getpass
 from time import sleep
 
-address = '192.168.0.2'
+address = '192.168.1.25'
 username = 'admin'
 password = 'Swnu*hgt'
+ip = input('Para qual IP deseja configurar o equipamento: ')
 
 # Fazendo a conexão
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname=address, username=username, password=password)
 
-# Executando comando
-stdin, stdout, stderr = ssh.exec_command("enable")
-sleep(.5)
-stdin.close()
+channel = ssh.invoke_shell()
+out = channel.recv(9999)
 
-print(stdout.readlines())
+# Executando comando
+comands = [
+
+]
+
+for command in comands:
+  channel.send(f'{command}\n')
+  sleep(.5)
+
+while not channel.recv_ready():
+    sleep(2)
+
+out = channel.recv(9999)
+print(out.decode("ascii"))
 
 # Encerrando conexão
 ssh.close()
